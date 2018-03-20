99ec4b0fc75a44b2dd1b37f3bfb363644274b551
NeveJJ <wjj8251107@hotmail.com>
2018-03-20 22:06:55 +0800

    github认证demo

---
+++ GraduateStudio/src/main/java/com/spc/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.spc.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableOAuth2Sso // 实现基于OAuth2的单点登录，建议跟踪进代码阅读以下该注解的注释，很有用
+//@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.
+                antMatcher("/**")
+                // 所有请求都得经过认证和授权
+                .authorizeRequests().anyRequest().authenticated()
+                .and().authorizeRequests().antMatchers("/", "/anon").permitAll()
+                .and()
+                // 这里之所以要禁用csrf，是为了方便。
+                // 否则，退出链接必须要发送一个post请求，请求还得带csrf token
+                // 那样我还得写一个界面，发送post请求
+                .csrf().disable()
+                // 退出的URL是/logout
+                .logout().logoutUrl("/logout").permitAll()
+                // 退出成功后，跳转到/路径。
+                .logoutSuccessUrl("/login");
+    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("666").password("666").roles("USER");
+//    }
+}
+
+
--- graduateStudio/src/main/java/com/spc/Application.java
+++ graduateStudio/src/main/java/com/spc/Application.java
@@ -6,15 +6,50 @@ package com.spc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * @author lc
- * 启动类，对于每一个微服务都一样
- */
+import java.security.Principal;
+
+
 @SpringBootApplication
 @MapperScan("com.spc")
+@EnableSwagger2
+@EnableZuulProxy
+@RestController
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome() {
+		return "welcome";
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	/**Swagger-ui*/
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(
+					new ApiInfoBuilder()
+					.title("商店管理接口").description("商店管理接口")
+					.version("1.0").build()
+				).select()
+				.apis(RequestHandlerSelectors.basePackage("com.spc")).paths(PathSelectors.any())
+				.build();
 	}
 }
