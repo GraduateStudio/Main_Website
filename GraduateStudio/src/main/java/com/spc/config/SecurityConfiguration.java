3e2105e23892a1da4fc26c3044f5dd49bb43d7dd
NeveJJ <wjj8251107@hotmail.com>
2018-03-21 22:38:59 +0800

    自定义页面权限控制

--- GraduateStudio/src/main/java/com/spc/config/SecurityConfiguration.java
+++ GraduateStudio/src/main/java/com/spc/config/SecurityConfiguration.java
@@ -1,17 +1,48 @@
 package com.spc.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Component;
 
 
-@Component
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 设定用户访问权限
+     * 用户身份可以访问 订单相关API
+     * */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/stm/user").hasRole("ADMIN")    //用户权限
+                .antMatchers("/stm/**").hasRole("USER")    //管理员权限
+                .antMatchers("/stm/wjj.html").hasRole("USER")    //管理员权限
+                .and()
+                .formLogin()
+                .loginPage("/stm/login").permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .csrf().disable();        //暂时禁用CSRF，否则无法提交表单
+    }
+    /**
+     * 添加自定义用户
+     * */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("666").password("666").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN","USER")
+                .and()
+                .withUser("terry").password("terry").roles("USER")
+                .and()
+                .withUser("larry").password("larry").roles("USER");
     }
+
 }
 
 
