fabb226b6933e85570fc3b1146c8c2d8e294ce7e
wjj8251107@hotmail.com <wjj8251107@hotmail.com>
2018-04-17 20:20:07 +0800

    增加netty websocket支持

--- graduateStudio/src/main/java/com/spc/Application.java
+++ graduateStudio/src/main/java/com/spc/Application.java
@@ -1,8 +1,16 @@
 /**
- * 
+ *
  */
 package com.spc;
 
+import com.spc.websocket.WebSocketChannelInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,17 +27,38 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.spc")
 @EnableSwagger2
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	@Bean
-	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(
-				new ApiInfoBuilder()
-						.title("商店管理接口").description("商店管理接口")
-						.version("1.0").build()
-		).select()
-				.apis(RequestHandlerSelectors.basePackage("com.spc")).paths(PathSelectors.any())
-				.build();
-	}
+    public static void main(String[] args) {
+        new Thread(() -> bind(8989)).start();
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(
+                new ApiInfoBuilder()
+                        .title("商店管理接口").description("商店管理接口")
+                        .version("1.0").build()
+        ).select()
+                .apis(RequestHandlerSelectors.basePackage("com.spc")).paths(PathSelectors.any())
+                .build();
+    }
+
+    private static void bind(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketChannelInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
