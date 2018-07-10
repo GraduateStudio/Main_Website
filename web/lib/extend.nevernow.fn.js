913f96229133657be9e7446025bb31cdcad4bd6c
wangjunjie <wangjunjie@lacesar.com>
2018-07-10 22:34:46 +0800

    根据文本生成页面和二维码

---
+++ web/lib/extend.nevernow.fn.js
@@ -0,0 +1,18 @@
+function getUrlParam(name) {
+
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var URL = decodeURI(window.location.search);
+    var r = URL.substr(1).match(reg);
+    if (r != null) {
+        //decodeURI() 函数可对 encodeURI() 函数编码过的 URI 进行解码
+        return decodeURI(r[2]);
+    }
+    return null;
+}
+
+Array.prototype.contains = function ( needle ) {
+    for (i in this) {
+        if (this[i] == needle) return true;
+    }
+    return false;
+}
